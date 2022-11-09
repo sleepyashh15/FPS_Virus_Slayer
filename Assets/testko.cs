c4cf988710a02831e5769aff7f62bc2d302260c0
sleepyashh15 <108870135+sleepyashh15@users.noreply.github.com>
2022-11-09 20:47:17 +0800

    SWITch pc add dialogue

---
+++ Assets/testko.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testko : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+   public void test()
+    {
+        Debug.Log("tst");
+    }
+}
